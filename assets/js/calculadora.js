5a1298602ce324422601e4e2875efc618175342d
Sanmir Cruz <sanmir.cruz@gmail.com>
2021-09-15 11:55:48 -0300

    Alterações realizadas: 1. Mais elementos para a ambientação (tendo interações e até funcionalidades); 2. Redução de código html, com o sendo varrido todos os elementos 'span', automatizando a ciração e usos de classes dessa tag html; 3. Adição de alguns ícones da biblioteca fontawesome v5.15.3; 4. Paleta de cores melhorada.

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,8 +1,22 @@
+// DEFININDO CLASSE PARA CADA span
+const spans = document.querySelectorAll('span')
+spans.forEach((item) => { item.classList.add("num") })
+
+simbolos = [
+   spans[0], spans[1], spans[2], spans[3], 
+   spans[4], spans[5], spans[9], spans[10], 
+   spans[14], spans[15], spans[19],spans[23]
+]
+simbolos.forEach((item) => { item.classList.add("simbolos") })
+
+// "Realizando cálculos que a função eval não realiza sozinha
+// (com exceção do cálculo de resto (%))."
 function calcula() {
    let valor = calc.txt.value.split(' ')
+   let resultado = calc.txt.value
 
    if (valor[1] === '%') {
-      let equacao = (valor[0] * (valor[2] / 100)).toFixed(3)
+      let equacao = (valor[0] * (valor[2] / 100)).toFixed(4)
       return eval(equacao)
 
    } if (valor[0] === '' && valor[1] === '√') { // ex: √ 9 = 3
@@ -10,26 +24,45 @@ function calcula() {
       return eval(equacao)
 
    } if (valor[0] === valor[0] && valor[1] === '√') { // ex: 2 √ 9 = 6 
-      let equacao = (valor[0] * (Math.sqrt(valor[2]))).toFixed(3)
+      let equacao = (valor[0] * (Math.sqrt(valor[2]))).toFixed(4)
       return eval(equacao)
 
    } if (valor[1] === '^') {
-      let equacao = (valor[0] ** valor[2]).toFixed(3)
+      let equacao = (valor[0] ** valor[2]).toFixed(4)
       return eval(equacao)
 
    } if (valor[1] === 'log') {
-      let equacao = (Math.log10(valor[2])).toFixed(3)
+      let equacao = (Math.log10(valor[2])).toFixed(4)
       return eval(equacao)
 
    } if (valor[1] === '(%)') {
-      let equacao = (valor[0] % valor[2]).toFixed(3)
+      let equacao = (valor[0] % valor[2]).toFixed(4)
       return eval(equacao)
 
+   } if (valor[1] === '/') {
+      let equacao = (valor[0] / valor[2]).toFixed(4)
+      return eval(equacao)
 
-   } else { return eval(calc.txt.value); }
+   } else { return eval(resultado); }
 }
+
 
 function del() {
    let num = calc.txt.value.split('  ').toString()
    return num.substring(0, (num.length - 1))
+}
+
+
+function expandir() {
+   const calcValores = document.querySelector('.value').classList.toggle("ativo")
+   const conteudos = document.querySelectorAll('.num')
+
+   const array = []
+   conteudos.forEach((item) => { array.push(item)})
+   
+   const conteudoExpandido = [
+      array[20], array[19],array[15],
+      array[10], array[5], array[0]
+   ]
+   conteudoExpandido.forEach((item) => { item.classList.toggle("ativo") })
 }
